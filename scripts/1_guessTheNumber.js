2b2c925f0f86799de76cc76ac7ee5da8944c884b
Ryan Kent <imsolost@gmail.com>
2017-02-09 16:46:59 -0800

    finished base fib

--- scripts/1_guessTheNumber.js
+++ scripts/1_guessTheNumber.js
@@ -1,23 +1,14 @@
 
 //learn readline rl.question
 
-exports.wordAtIndex = function(string, number) {
-  let splitString = string.replace(/(?!\w|\s)./g, '').split(' ')
+exports.guessingGame = function() {
+  let randomNumber =  Math.floor(Math.random() * 10 + 1)
+  let correctGuess = false
 
-  if (splitString[number - 1]) {
-    return splitString[number - 1]
-  }
-  return ''
-}
+  console.log(randomNumber)
 
-function guessingGame() {
-  var randomNumber =  Math.floor(Math.random() * 10 + 1)
-  let hasGuessedCorrectly = false
-
-  console.log("I have thought of a random number in my brain!")
-
-  while (!hasGuessedCorrectly) {
-    var guess = prompt('What is your guess?')
+  while (!correctGuess) {
+    let guess = prompt('What is your guess?')
 
     if (guess > randomNumber) {
       console.log('you guessed too high')
@@ -25,37 +16,12 @@ function guessingGame() {
     if (guess < randomNumber) {
       console.log('you guessed too low');
     }
-    if (guess === randomNumber) {
-      console.log('you guessed just right');
-      hasGuessedCorrectly = true
-      break
+    if (guess == randomNumber) {
+      correctGuess = true
+      console.log('you guessed correctly')
     }
     if (guess === 'exit') {
       break
     }
   }
 }
-
-// var randomNumber = Math.floor((Math.random() * 100) + 1);
-//
-// print("I have thought of a random number in the range of 1 to 100. Guess!");
-//
-// var hasGuessedCorrectly = false;
-//
-// while (!hasGuessedCorrectly)
-// {
-//     var guess = readline();
-//     if (guess < randomNumber)
-//     {
-//         print("Lower");
-//     }
-//     else if (guess > randomNumber)
-//     {
-//         print("Higher");
-//     }
-//     else if (guess == randomNumber)
-//     {
-//         print("Good Job");
-//         hasGuessedCorrectly = true;
-//     }
-// }
--- test/1_guessTheNumberTest.js
+++ test/1_guessTheNumberTest.js
@@ -1,11 +1,11 @@
-var expect    = require("chai").expect;
-var scrabBag = require("../scripts/2_scrabBag");
-
-describe("scrabBag test", function() {
-  it("should return the remaining tiles", function() {
-    var themTiles  = scrabBag.remainingTiles('AERETOXMYCNS_B');
-
-    expect(themTiles).to.equal('10: E\n9: I\n8: A\n7: O\n5: N, R, T\n4: D, L, U\n3: G, S\n2: F, H, P, V, W\n1: B, C, J, K, M, Q, Y, Z, _\n0: X');
-  });
-
-});
+// var expect    = require("chai").expect;
+// var guessTheNumber = require("../scripts/guessTheNumber");
+//
+// describe("scrabBag test", function() {
+//   it("should return the remaining tiles", function() {
+//     var game  = guessTheNumber.guessingGame();
+//
+//     expect(game).to.equal();
+//   });
+//
+// });
